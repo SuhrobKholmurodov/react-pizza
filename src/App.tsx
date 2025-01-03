b1e236fe1e01d7be737c7191ca68156e69ebe782
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-03 21:57:41 +0500

    fix: 'React' is declared but its value is never read.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import MainLayout from './layouts/MainLayout';
-// const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
-import NotFound from './pages/NotFound';
+import { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import MainLayout from './layouts/MainLayout'
+import NotFound from './pages/NotFound'
 
-function App() {
+function App () {
   return (
-  <div className='pl-[5%] dark:bg-[#1b1b1f] pr-[5%]'>
+    <div className='pl-[5%] dark:bg-[#1b1b1f] pr-[5%]'>
       <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-        <Route
-          path="cart"
-          element={
-            <Suspense fallback={<div>Идёт загрузка корзины...</div>}>
-              <Cart />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<div>Идёт загрузка...</div>}>
-              <NotFound />
-            </Suspense>
-          }
-        />
-      </Route>
-    </Routes>
-  </div>
-  );
+        <Route path='/' element={<MainLayout />}>
+          <Route path='' element={<Home />} />
+          <Route
+            path='cart'
+            element={
+              <Suspense fallback={<div>Идёт загрузка корзины...</div>}>
+                <Cart />
+              </Suspense>
+            }
+          />
+          <Route
+            path='*'
+            element={
+              <Suspense fallback={<div>Идёт загрузка...</div>}>
+                <NotFound />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
