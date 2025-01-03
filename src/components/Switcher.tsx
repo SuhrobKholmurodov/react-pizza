fbe8cb86b5b4cf66d0cd32db3a69d034d2ee6f7d
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-03 15:52:45 +0500

    Add dynamic tooltip message for theme toggle

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Sun, Moon } from 'lucide-react'
 import useDarkSide from '../hooks/useDarkSide'
+import { Tooltip } from '@mui/material'
 
 export default function Switcher () {
   const [colorTheme, setTheme] = useDarkSide()
@@ -15,53 +16,42 @@ export default function Switcher () {
 
   return (
     <div className='relative'>
-      <div
-        className='relative flex items-center justify-center'
-        style={{
-          width: '45px',
-          height: '45px',
-          position: 'relative',
-          borderRadius: '50%',
-          backgroundColor: 'transparent'
-        }}
-      >
-        <Sun
-          size={35}
-          color='#f39c12'
-          className={`absolute transition-opacity duration-500 ease-in-out ${
-            darkSide ? 'opacity-0' : 'opacity-100'
-          }`}
-          style={{
-            transform: darkSide ? 'rotate(180deg)' : 'rotate(0deg)',
-            transition: 'transform 0.5s ease-in-out'
-          }}
-        />
-        <Moon
-          size={34}
-          color='#9b9b9b'
-          className={`absolute transition-opacity duration-500 ease-in-out ${
-            darkSide ? 'opacity-100' : 'opacity-0'
-          }`}
-          style={{
-            transform: darkSide ? 'rotate(0deg)' : 'rotate(-180deg)',
-            transition: 'transform 0.5s ease-in-out'
-          }}
-        />
-
+      <Tooltip title={darkSide ? 'Switch to light' : 'Switch to dark'} arrow>
         <div
-          onClick={() => toggleDarkMode(!darkSide)}
+          className='relative hover:cursor-pointer flex items-center justify-center'
           style={{
-            width: '50px',
-            height: '50px',
-            position: 'absolute',
-            top: 0,
-            left: 0,
+            width: '45px',
+            height: '45px',
+            position: 'relative',
             borderRadius: '50%',
-            backgroundColor: 'transparent',
-            cursor: 'pointer'
+            backgroundColor: 'transparent'
           }}
-        />
-      </div>
+          onClick={() => toggleDarkMode(!darkSide)}
+        >
+          <Sun
+            size={35}
+            color='#f39c12'
+            className={`absolute transition-opacity duration-500 ease-in-out ${
+              darkSide ? 'opacity-0' : 'opacity-100'
+            }`}
+            style={{
+              transform: darkSide ? 'rotate(180deg)' : 'rotate(0deg)',
+              transition: 'transform 0.5s ease-in-out'
+            }}
+          />
+          <Moon
+            size={34}
+            color='#9b9b9b'
+            className={`absolute transition-opacity duration-500 ease-in-out ${
+              darkSide ? 'opacity-100' : 'opacity-0'
+            }`}
+            style={{
+              transform: darkSide ? 'rotate(0deg)' : 'rotate(-180deg)',
+              transition: 'transform 0.5s ease-in-out'
+            }}
+          />
+        </div>
+      </Tooltip>
     </div>
   )
 }
