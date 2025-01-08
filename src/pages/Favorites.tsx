7ddb7d64aa1e3fd52dd047ef0da575f3c9dc31f0
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-08 23:58:33 +0500

    feat: add mobile bottom bar with cart display and home button in favorites page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Trash2, ArrowLeft } from 'lucide-react'
+import { Trash2, ArrowLeft, ShoppingCart, Home } from 'lucide-react'
 import { selectFavorites } from '../redux/favorites/selectors'
+import { selectCart } from '../redux/cart/selectors'
 import { clearFavorites } from '../redux/favorites/slice'
 import { FavoritesEmpty, FavoritesItem } from '../components/Favorites'
 import { DialogDelete } from '../components'
 
 const Favorites = () => {
   const dispatch = useDispatch()
-  const { items } = useSelector(selectFavorites)
+  const { items: favoriteItems } = useSelector(selectFavorites)
+  const { items: cartItems, totalPrice } = useSelector(selectCart)
   const [openDialog, setOpenDialog] = useState(false)
+
+  const totalCount = cartItems.reduce((sum, item) => sum + item.count, 0)
 
   const handleOpenDialog = () => {
     setOpenDialog(true)
@@ -22,6 +26,7 @@ const Favorites = () => {
     dispatch(clearFavorites())
     setOpenDialog(false)
   }
+
   return (
     <div className='dark:bg-mainBgColor dark:text-mainTextColor min-h-screen'>
       <div className='flex items-center gap-2 mb-4'>
@@ -34,7 +39,7 @@ const Favorites = () => {
           <h1 className='text-3xl sm:text-[20px] font-bold text-start'>
             My Favorites
           </h1>
-          {items.length > 0 && (
+          {favoriteItems.length > 0 && (
             <button
               onClick={handleOpenDialog}
               className='flex items-center gap-2 text-red-500 hover:text-red-600 transition duration-300'
@@ -46,9 +51,9 @@ const Favorites = () => {
           )}
         </div>
       </div>
-      {items.length > 0 ? (
+      {favoriteItems.length > 0 ? (
         <div className='grid grid-cols-4 sm:grid-cols-1 gap-6'>
-          {items.map(item => (
+          {favoriteItems.map(item => (
             <FavoritesItem key={item.id} item={item} />
           ))}
         </div>
@@ -57,6 +62,30 @@ const Favorites = () => {
           <FavoritesEmpty />
         </div>
       )}
+
+      <div className='px-[5%] dark:text-mainTextColor sm:pl-[5%] fixed left-0 right-0 sm:flex-row bottom-0 dark:bg-mainBgColor dark:border-t-[black] p-4 border-t bg-gray-100 flex justify-center sm:justify-between items-center'>
+        <Link
+          to='/'
+          className='hidden sm:flex items-center gap-2 text-black dark:text-mainTextColor hover:text-gray-600 dark:hover:text-gray-300 transition duration-300'
+        >
+          <Home size={20} />
+          <span className='font-[500]'>Home</span>
+        </Link>
+
+        <div className='hidden sm:flex border dark:border-[#0d0d0d] dark:border-2 rounded-lg sm:px-[15px] px-[25px]'>
+          <Link to='/cart' className='flex py-[12px] gap-[10px]'>
+            <span className='font-[600]'>
+              {totalPrice} <span className='font-[800]'>$</span>
+            </span>
+            <div className='h-[100%] w-[2.5px] dark:bg-mainTextColor bg-black'></div>
+            <div className='flex gap-[3px] items-center'>
+              <ShoppingCart className='sm:text-[12px]' />
+              <span className='font-[500] sm:font-[400]'>{totalCount}</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+
       <DialogDelete
         open={openDialog}
         onClose={handleCloseDialog}
