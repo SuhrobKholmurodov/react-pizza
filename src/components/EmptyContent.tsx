aa51d7a7228f9f045cdaff5d9704d6d0fc7e182c
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-11 00:29:34 +0500

    delete(EmptyContent): image from the props

--- src/components/EmptyContent.tsx
+++ src/components/EmptyContent.tsx
@@ -2,29 +2,20 @@ import { BackToHomeButton } from './BackToHomeButton'
 interface EmptyContentProps {
   title: string
   subtitle: string
-  imageSrc?: string
   showButton?: boolean
 }
 export const EmptyContent = ({
   title,
   subtitle,
-  imageSrc,
   showButton
 }: EmptyContentProps) => (
   <div className='flex text-center justify-center h-[100vh] sm:h-[100%] items-center dark:text-mainTextColor mt-[-10%] sm:mt-0 gap-[20px] flex-col'>
-    <h2 className='text-[40px] sm:text-[30px] sm:mt-[100px] font-[600]'>
+    <h2 className='text-[40px] sm:text-[27px] sm:mt-[100px] font-[600]'>
       {title}
     </h2>
-    <p className='text-[#a6a5a5] text-[20px] w-1/2 sm:w-full font-[500]'>
+    <p className='text-[#a6a5a5] text-[20px] sm:text-[18px] w-1/2 sm:w-full font-[500]'>
       {subtitle}
     </p>
-    {imageSrc && (
-      <img
-        src={imageSrc}
-        alt={'error getting image'}
-        className='w-[370px] sm:w-[280px]'
-      />
-    )}
     {showButton && <BackToHomeButton />}
   </div>
 )
