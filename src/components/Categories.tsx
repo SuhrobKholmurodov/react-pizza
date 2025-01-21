307f4263555bf213c52b017619857ae696000359
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-22 03:32:14 +0500

    refactor: replace category list with Swiper slider

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useLocalization } from '../hooks'
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 
 type CategoriesProps = {
   value: number
@@ -21,24 +25,31 @@ export const Categories = React.memo(
     const { t } = useLocalization()
 
     return (
-      <div className='relative max-w-full overflow-x-auto'>
-        <ul className='flex space-x-4 list-none py-2 px-[2px]'>
+      <div className='relative max-w-full'>
+        <Swiper
+          slidesPerView={'auto'}
+          spaceBetween={16}
+          centeredSlides={false}
+          modules={[Autoplay, Pagination, Navigation]}
+          className='w-full sm:pr-[3px] bg-transparent'
+        >
           {categoryKeys.map((categoryKey, i) => (
-            <li
-              key={i}
-              onClick={() => onChangeCategory(i)}
-              className={`cursor-pointer rounded-full transition-colors duration-200 
-                px-4 py-2 text-center flex items-center justify-center
-                ${
-                  value === i
-                    ? 'bg-black dark:bg-gray-700 text-white'
-                    : 'bg-gray-200 dark:text-mainTextColor dark:bg-[#202127] hover:text-white hover:dark:bg-[#252a34] hover:bg-[#1f1e1e]'
-                }`}
-            >
-              {t(categoryKey)}
-            </li>
+            <SwiperSlide key={i} className='!w-auto !bg-transparent'>
+              <div
+                onClick={() => onChangeCategory(i)}
+                className={`cursor-pointer rounded-full transition-colors duration-200 
+                  px-4 py-2 text-center flex items-center text-[15px] justify-center
+                  ${
+                    value === i
+                      ? 'bg-black dark:bg-gray-700 text-white'
+                      : 'dark:text-mainTextColor bg-gray-300 dark:bg-[#202127] hover:text-white hover:dark:bg-[#252a34] hover:bg-[#1f1e1e]'
+                  }`}
+              >
+                {t(categoryKey)}
+              </div>
+            </SwiperSlide>
           ))}
-        </ul>
+        </Swiper>
       </div>
     )
   }
