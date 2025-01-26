ca32e538665dd9cb3943d885bc234ebb37d629ff
Kholmurodov <suhrobkholmurodov1@gmail.com>
2025-01-26 23:28:09 +0500

    feat(PriceFilter): add localization for both inputs

--- src/components/PriceFilter.tsx
+++ src/components/PriceFilter.tsx
@@ -1,14 +1,19 @@
+import { useLocalization } from '../hooks'
+
 export const PriceFilter = () => {
+  const { t, lng } = useLocalization()
   return (
     <div className='flex justify-between gap-[20px]'>
       <div className='relative'>
         <input
           type='number'
           placeholder='139'
-          className='w-[160px] px-3 py-2 border focus:ring-slate-200 dark:focus:ring-slate-800 focus:ring-2 border-gray-300 rounded-md dark:bg-gray-900 dark:text-white dark:border-gray-600 pl-10 no-spinners'
+          className={`w-[160px] px-3 py-2 border focus:ring-slate-200 dark:focus:ring-slate-800 focus:ring-2 border-gray-300 rounded-md dark:bg-gray-900 dark:text-white dark:border-gray-600 no-spinners ${
+            lng === 'en' ? 'pl-14' : 'pl-10'
+          }`}
         />
-        <span className='absolute left-2 top-[20px] transform -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none'>
-          От:
+        <span className='absolute left-2 top-[19px] transform -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none'>
+          {t('priceFilter.from')}:
         </span>
       </div>
       <div className='relative'>
@@ -17,8 +22,8 @@ export const PriceFilter = () => {
           placeholder='190'
           className='w-[160px] px-3 py-2 border focus:ring-slate-200 dark:focus:ring-slate-800 focus:ring-2 border-gray-300 rounded-md dark:bg-gray-900 dark:text-white dark:border-gray-600 pl-10 no-spinners'
         />
-        <span className='absolute left-2 top-[20px] transform -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none'>
-          До:
+        <span className='absolute left-2 top-[19px] transform -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none'>
+          {t('priceFilter.to')}:
         </span>
       </div>
 
